16e67c40c21fbd347f06a713fdbd257c84f997f8
houdaslassi <houdaslassi.18@gmail.com>
2025-06-04 23:32:02 +0100

    feat: add Core Database class

---
+++ app/Core/Database.php
@@ -0,0 +1,31 @@
+<?php
+namespace Core;
+
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        try {
+            $this->connection = new \PDO(
+                "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+                DB_USER,
+                DB_PASS,
+                [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (\PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+} 
