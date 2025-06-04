e88d06f620bca39e03009e9d7f24384d3aafae63
houdaslassi <houdaslassi.18@gmail.com>
2025-06-04 23:32:53 +0100

    feat: add home index view

---
+++ app/views/home/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $title ?></title>
+</head>
+<body>
+    <h1><?= $title ?></h1>
+    <p><?= $message ?></p>
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/about">About</a> |
+        <a href="/test">Test API</a>
+    </nav>
+</body>
+</html> 
